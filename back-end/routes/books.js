11b1149b2013ba7deee80f4d5547af09ca7c6872
rachhind <rachael.hind77@gmail.com>
2025-07-11 18:20:53 -0400

    feat: Add functionality to manage book availability and borrowing status

--- back-end/routes/books.js
+++ back-end/routes/books.js
@@ -24,8 +24,15 @@ router.get('/getBooks', (req, res)=>{
         /* implement the getBooks route from scratch */
 });
 
-router.get('/getBook/:id', (req, res)=>{
-       /* implement the getBook route from scratch */
+router.get('/getBook/:id', (req, res) => {
+    Book.findOne({ where: { ID: req.params.id } })
+        .then(book => {
+            if (!book) {
+                return res.status(404).json({ error: 'Book not found' });
+            }
+            return res.status(200).json(book);
+        })
+        .catch(err => res.status(500).json({ error: err.message }));
 });
 
 router.patch('/changeBook/:id', (req, res)=>{
