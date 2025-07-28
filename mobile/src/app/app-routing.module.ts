a6447906264fcddd9fc4407c458d8127925195c2
rachhind <rachael.hind77@gmail.com>
2025-07-28 17:20:57 -0400

    feat: Add Tab1, Tab2, and Tab3 modules with corresponding components and templates

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
-  { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksPageModule) }
+  {
+    path: '',
+    redirectTo: 'tabs/tab1',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-
-
